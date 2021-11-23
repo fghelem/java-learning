7561005febefa0d6bacae00cc93311105d076ccc
fghelem <94249797+fghelem@users.noreply.github.com>
2021-11-24 00:09:42 +0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -21,4 +21,93 @@ public int add(int firstnum, int secnum){
  Integer myint = null;
  Integer bint =myint * 2;
  return bint;
+ public static void main(String args[])  
+{ 
+ int a=0;
+ string name ="name"; 
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+ #username = testttt
+ #password = password
+  
+}
+public int add(int firstnum, int secnum){
+ Integer myint = null;
+ Integer bint =myint * 2;
+ return bint;
+ public static void main(String args[])  
+{ 
+ int a=0;
+ string name ="name"; 
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+ #username = testttt
+ #password = password
+  
+}
+public int add(int firstnum, int secnum){
+ Integer myint = null;
+ Integer bint =myint * 2;
+ return bint;
+ public static void main(String args[])  
+{ 
+ int a=0;
+ string name ="name"; 
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+ #username = testttt
+ #password = password
+  
+}
+public int add(int firstnum, int secnum){
+ Integer myint = null;
+ Integer bint =myint * 2;
+ return bint;
+ public static void main(String args[])  
+{ 
+ int a=0;
+ string name ="name"; 
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+ #username = testttt
+ #password = password
+  
+}
+public int add(int firstnum, int secnum){
+ Integer myint = null;
+ Integer bint =myint * 2;
+ return bint;
+ 
 }
