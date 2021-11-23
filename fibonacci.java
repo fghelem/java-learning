348687f3787c2ad67e275165eeca472b63a402ba
fghelem <94249797+fghelem@users.noreply.github.com>
2021-11-23 21:10:42 +0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,6 +1,8 @@
 class FibonacciExample1{  
 public static void main(String args[])  
-{    
+{ 
+ int a=0;
+ string name ="name"; 
  int n1=0,n2=1,n3,i,count=10;    
  System.out.print(n1+" "+n2);//printing 0 and 1    
     
